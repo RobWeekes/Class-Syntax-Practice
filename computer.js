e96920542c1dc9a309b4a15ab8f208d2536c1f6d
robweekes <weekes.robert@gmail.com>
2024-05-28 17:58:02 -0700

    Computer class and methods

---
+++ computer.js
@@ -0,0 +1,34 @@
+class Computer {
+    constructor(brand, OS, type) {
+        this.brand = brand;
+        this.OS = OS;
+        this.type = type;
+    }
+
+    // INSTANCE methods
+    powerOn() {
+        return `${this.brand} ${this.type} powers on with ${this.OS}.`;
+    }
+
+    // STATIC CLASS methods
+    static bootUpPCs(computerArray) {
+        let result = [];
+        computerArray.forEach(PC => {
+            result.push(PC.powerOn());
+        });
+        return result;
+    }
+}
+
+let macbook = new Computer('Apple', 'OSX', 'laptop');
+// console.log(macbook);
+// console.log(macbook.powerOn());
+
+let dell = new Computer('Dell', 'Windows', 'desktop')
+// console.log(dell);
+// console.log(dell.powerOn());
+
+let pcArr = [macbook, dell];
+// console.log(pcArr);
+
+console.log(Computer.bootUpPCs(pcArr));
